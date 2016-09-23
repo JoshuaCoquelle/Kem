e6edbd33c16fce17fd8ba25ccf93b2cd0df1ec2e
JoshuaCoquelle <joshuacoquelle@gmail.com>
2016-09-22 23:47:05 -0400

    create new api methods + refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 /*
-|---------------------------------------------------------------
-|           Kem :: Dependency Free Event Emitting
-|---------------------------------------------------------------
+| -----------------------------------------------------------------
+| Plugin: Kem.js
+| Description: Dependency free JavaScript event emitter
+| Version: 1.0.0
+| Author: Josua Coquelle
+| September 2016
+| License: MIT
+| -----------------------------------------------------------------
 */
-var Kem = (function() {
 
+var Kem = (function() {
     /**
      * Kem constructor.
      */
@@ -13,49 +18,66 @@ var Kem = (function() {
     }
 
     /*
-    |---------------------------------------------------------------
-    | Main Kem instance methods to be used by library user. ($)
-    |---------------------------------------------------------------
+    | -----------------------------------------------------------------
+    | ~ Public ~ :: Kem API methods.
+    | -----------------------------------------------------------------
     */
-
+    
     /**
-     * Creates or attaches a callback function to an existing event.
+     * Push an event callback to an existing or newly created event.
      * 
-     * @param {String}   eventName | Name of event to create/existing event. 
-     * @param {Function} callback  | Callback function to attach to event.
-     * @returns
+     * @param  {String}   event    | Name of event to create/existing event. 
+     * @param  {Function} callback | Callback function to attach to event.
+     * @return {Object}            | Returns Kem instance.
      */
-    Kem.prototype.$when = function(eventName, callback) {
-        setOrCreateEvent(this, eventName);
+    Kem.prototype.when = function(event, callback) {
+        if (functionIsAnonymous(event, callback)) return;
 
-        var token = generateToken();
+        setOrCreateEventKey(this, event);
 
-        pushToEvent(this, eventName, {
-            token: token,
+        this.events[event].push({
             callback: callback,
             callbackName: callback.name
         });
 
-        return token;
+        return this;
     };
-
+    
     /**
-     * Trigger the specified event and pass along the desired data.
+     * Wire multiple events at once by passing an event object literal.
      * 
-     * @param {String} eventName | Name of existing event.
-     * @param {any}    data      | Data to be passed to the events callback.
-     * @returns
+     * @param  {Object} eventObj | Object literal to register event key value pairs.
+     * @return {Object}          | Returns Kem instance.
      */
-    Kem.prototype.$send = function(eventName, data) {
-        if (!eventExists(this, eventName)) {
-            return;
+    Kem.prototype.wire = function(eventObj) {
+        for (var event in eventObj) {
+            if (valueIsNotArrayOrFunction(eventObj[event])) return;
+
+            var callbacks = [].concat(eventObj[event]);
+
+            setOrCreateEventKey(this, event);
+
+            registerEventCallbacks(this, event, callbacks);
         }
 
-        var event = this.events[eventName];
-        var props = event ? event.length : 0;
+        return this;
+    };
+    
+    /**
+     * Fire the specified event and pass the desired data to callback.
+     * 
+     * @param  {String} event | Name of existing event.
+     * @param  {any}    data  | Data to be passed to the events callback.
+     * @return {Object}       | Returns Kem instance.
+     */
+    Kem.prototype.fire = function(event, data) {
+        if (!eventExists(this, event)) return;
 
-        while (props--) {
-            event[props].callback(data, eventName);
+        var event = this.events[event];
+        var callbacks = event ? event.length : 0;
+
+        while (callbacks--) {
+            event[callbacks].callback(data, event);
         }
 
         return this;
@@ -64,18 +86,17 @@ var Kem = (function() {
     /**
      * Remove stored callback from the specificed event.
      *
-     * @param {String}   eventName | Name of existing event.
-     * @param {Variable} fnVar     | Variable used to save $when() functon call.
+     * @param  {String} event    | Name of event to target.
+     * @param  {String} callback | Function name to be removed from targeted event
+     * @return {Object}          | Returns Kem instance.
      */
-    Kem.prototype.$stop = function(eventName, name) {
-        if (!eventExists(this, eventName)) {
-            return;
-        }
+    Kem.prototype.stop = function(event, callback) {
+        if (!eventExists(this, event)) return;
 
-        var event = this.events[eventName];
+        var event = this.events[event];
 
         event.forEach(function(key, index) {
-            if (key.token === name) {
+            if (key.callbackName === callback) {
                 event.splice(index, 1);
             }
         });
@@ -84,95 +105,167 @@ var Kem = (function() {
     };
 
     /**
-     * Empty all callbacks from the specified event. 
+     * Empty all callbacks from the targeted event. 
      * 
-     * @param {String} eventName
-     * @returns
+     * @param  {String} event | Name of event to target.
+     * @return {Object}       | Returns Kem instance.
      */
-    Kem.prototype.$empty = function(eventName) {
-        if (eventExists(this, eventName)) {
-            this.events[eventName] = [];
+    Kem.prototype.empty = function(event) {
+        if (eventExists(this, event)) {
+            this.events[event] = [];
         }
 
         return this; 
     };
 
     /**
-     * Destroy an entire event from the events object.
+     * Delete an entire single event from the events object.
      *
-     * @param {String} eventName
-     * @returns
+     * @param  {String} event | Name of event to delete.
+     * @return {Object}       | Returns Kem instance.
      */
-    Kem.prototype.$destroy = function(eventName) {
-        if (eventExists(this, eventName)) {
-            delete this.events[eventName];
+    Kem.prototype.destroy = function(event) {
+        if (eventExists(this, event)) {
+            delete this.events[event];
         }
 
         return this;
     }
 
     /**
-     * Destroys ALL events from the events object.
+     * Delete all events from the instance events object.
      *
-     * @returns
+     * @return {Object} | Returns Kem instance.
      */
-    Kem.prototype.$destroyAll = function() {
+    Kem.prototype.destroyAll = function() {
         this.events = {};
 
         return this;
     };
 
-    /*
-    |---------------------------------------------------------------
-    | Helper utilty methods for inner class use.
-    |---------------------------------------------------------------
-    */
+    /**
+     * Allow API user to check if the current instance has the specified event.
+     * 
+     * @param  {String} event | Name of event to target.
+     * @return {Boolean}      | Returns whether or not the event exists.
+     */
+    Kem.prototype.hasEvent = function(event) {
+        return eventExists(this, event);
+    };
 
     /**
-     * Determine whether an event exist by key name.
+     * Determine whether the specified event has the existing callback.
      * 
-     * @param {String} eventName | A string containing the name of the event.
-     * @returns
+     * @param  {String}  event    | Name of event to target.
+     * @param  {String}  callback | String containing name of the callback function.
+     * @return {Boolean}          | Returns whether or not the specificed callback exists on the event.
      */
-    function eventExists(scope, eventName) {
-        if (scope.events[eventName]) {
-            return true;
+    Kem.prototype.hasCallback = function(event, callback) {
+        if (!eventExists(this, event)) return;
+
+        var event = this.events[event];
+
+        for (var i = 0; i < event.length; i++) {
+            if (event[i].callbackName === callback) return true;
         }
 
         return false;
+    };
+
+    /**
+     * Get the current instances event key names.
+     * 
+     * @return {Object} | Returns the events object literal.
+     */
+    Kem.prototype.getKeys = function() {
+        var eventKeys = [];
+
+        for (var event in this.events) {
+            eventKeys.push(event);
+        }
+
+        return eventKeys;
+    };
+
+    /*
+    | -----------------------------------------------------------------
+    | ~ Private ~ :: Helper utilty functions for inner class use.
+    | -----------------------------------------------------------------
+    */
+
+    /**
+     * Check for existance of the current passed event.
+     *
+     * @param  {Object}  scope | The object scope to evaluate.
+     * @param  {String}  event | Name of event to target.
+     * @return {Boolean}       | Return whether event exists or not.
+     */
+    function eventExists(scope, event) {
+        var isEvent = scope.events[event] ? true : false;
+
+        return isEvent;
     }
 
     /**
-     * Initialize a new event as an empty array or add to existing event.
+     * Determine whether the passed callback is an anonymous function.
      * 
-     * @param {String} eventName | A string containing the name of the event.
+     * @param  {Function} callback | Callback function to attach to event.
+     * @return {Boolean}           | Returns whether or not the function is anonymous.
      */
-    function setOrCreateEvent(scope, eventName) {
-        scope.events[eventName] = scope.events[eventName] || [];
+    function functionIsAnonymous(callback) {
+        var funcToString = callback.toString().split(' ').join('');
+
+        var isNamedFunction = (funcToString.indexOf('function()') > -1);
+
+        return isNamedFunction;
+    }
+
+    /**
+     * Determine whether or not the event value is an Array or a Function.
+     * 
+     * @param  {Function || Array} callback | Kem will only accept an array or function callback value.
+     * @return {Boolean}                    | Return whether or not the callback is valid.
+     */
+    function valueIsNotArrayOrFunction(callback) {
+        var callbackType = callback.constructor;
+
+        if (callbackType === Array || callbackType === Function) return false;
+
+        return true;
+    }
+    
+    /**
+     * If the event doesn't exist, create and initalize the value as an empty array.
+     * 
+     * @param  {Object} scope | The object context to evaluate.
+     * @param  {String} event | Name of event to target.
+     * @return {Object}       | Returns the current instance scope.
+     */
+    function setOrCreateEventKey(scope, event) {
+        scope.events[event] = scope.events[event] || [];
 
         return scope;
     }
 
     /**
-     * Push a properties object to the specified event.
+     * Loop through the callbacks array and register them to the event if validation passes.
      * 
-     * @param {String} eventName | A string containing the name of the event.
-     * @param {Object} props     | Push a callback props object to an event.
-     * @returns
+     * @param  {Object} scope     | The object context to evaluate.
+     * @param  {String} event     | Name of event to target.
+     * @param  {Array}  callbacks | An array containing the list of desired callbacks to register.
+     * @return {Object}           | Returns an instance of the current scope.
      */
-    function pushToEvent(scope, eventName, props) {
-        scope.events[eventName].push(props);
-        
-        return scope;
-    }
+    function registerEventCallbacks(scope, event, callbacks) {
+        for (var i = 0; i < callbacks.length; i++) {
+            if (functionIsAnonymous(callbacks[i])) return;
 
-    /**
-     * Generates a 13 character long alpha-numeric string token.
-     * 
-     * @returns
-     */
-    function generateToken() {
-        return Math.random().toString(16).slice(2);
+            scope.events[event].push({
+                callback: callbacks[i],
+                callbackName: callbacks[i].name
+            });
+        }
+
+        return scope;
     }
 
     /**
